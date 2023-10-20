a994cc71d9f48df6f48821e53b04ac8a7b4592c4
Rachel Cutler <rach_cutler@live.co.uk>
2023-10-20 12:48:27 +0000

    added the code to change the card on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,11 +35,19 @@ document.addEventListener("DOMContentLoaded", function () {
         card.appendChild(back);
         gameBoard.appendChild(card);
 
-        // Add click event listener to reveal whats on the card
-        card.addEventListener("click", function() {
-            card.classList.add("flip");
-        })
-    });
+// Add click event listener to reveal what's on the card or flip it back
+card.addEventListener("click", function () {
+    const icon = card.querySelector(".front i"); // Find the icon within the card
+
+    if (!card.classList.contains("flip")) {
+        card.classList.add("flip");
+        icon.className = gameCard; // Set the icon when the card is flipped
+    } else {
+        card.classList.remove("flip");
+        icon.className = "fas fa-question"; // Set the initial blank card icon when unflipped
+    }
+});
+});
 
     /**
      * Shuffle the Array
@@ -50,6 +58,46 @@ document.addEventListener("DOMContentLoaded", function () {
             [array[i], array[j]] = [array[j], array[i]];
         }
     }
+
+
+    function handleCardClick(event) {
+        const clickedCard = event.target;
+    
+        if (clickedCard.classList.contains('flip') || clickedCard.classList.contains('matched')) {
+            return; // Do nothing if the card is already flipped or matched
+        }
+    
+        // Add the logic to handle the click event here
+        // You can use the clickedCard variable to access the clicked card's properties
+    
+        // Example logic to check if two flipped cards match
+        flippedCards.push(clickedCard);
+        if (flippedCards.length === 2) {
+            const [card1, card2] = flippedCards;
+            if (card1.dataset.card === card2.dataset.card) {
+                // Cards match, mark them as "matched"
+                card1.classList.add('matched');
+                card2.classList.add('matched');
+                flippedCards = [];
+    
+                // Increment the matchedPairs count and check if the game is won
+                matchedPairs++;
+                if (matchedPairs === cardIcons.length / 2) {
+                    // Game won
+                    alert('Congratulations! You won the game.');
+                }
+            } else {
+                // Cards do not match, flip them back after a brief delay
+                setTimeout(() => {
+                    card1.classList.remove('flip');
+                    card2.classList.remove('flip');
+                    flippedCards = [];
+                }, 1000); // Delay for 1 second
+            }
+        }
+    }
+
+    
 
 
 
